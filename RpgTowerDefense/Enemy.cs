80031dcb1ec1ff5b56fca4c1bf3c56a3f261b7e3
Morten Ellegaard Hansen <mort140w@student.eadania.dk>
2018-05-21 17:56:06 +0200

    EnemyMovement

--- RpgTowerDefense/Enemy.cs
+++ RpgTowerDefense/Enemy.cs
@@ -3,37 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
 
-namespace RpgTowerDefense.bin
+namespace RpgTowerDefense
 {
-    class Enemy : Component
+    class Enemy : Component, ILoadable, IAnimateable, IUpdate
     {
         #region Fields
-        private float health;
-        private float attackPower;
-        private float movementSpeed;
+        private float speed;
+        private Animator animator;
+        private IStrategy strategy;
+        private DIRECTION direction;
         #endregion
-
-
-
-
-
-        #region Properties
-        public float MovementSpeed { get => movementSpeed; set => movementSpeed = value; }
-        public float AttackPower { get => attackPower; set => attackPower = value; }
-        public float Health1 { get => health; set => health = value; }
-        #endregion
-
-
-
-
-
-        #region Metoder
-        public Enemy(float health, float attackPower, float movementSpeed)
+        #region Constructor
+        public Enemy(GameObject gameobject) : base(gameobject)
         {
-            this.health = health;
-            this.attackPower = attackPower;
-            this.movementSpeed = movementSpeed;
+            speed = 50;
+            animator = (gameobject.GetComponent("Animator")as Animator);
+        }
+        #endregion
+        #region Methods
+        public void LoadContent(ContentManager Content)
+        {
+            CreateAnimation();
+        }
+        public void CreateAnimation()
+        {
+            animator.CreateAnimation("IdleFront", new Animation(1, 0, 0, 100, 100, 0, Vector2.Zero));
+            animator.CreateAnimation("IdleLeft", new Animation(1, 0, 1, 100, 100, 0, Vector2.Zero));
+            animator.CreateAnimation("IdleRight", new Animation(1, 0, 2, 100, 100, 0, Vector2.Zero));
+            animator.CreateAnimation("IdleBack", new Animation(1, 0, 3, 100, 100, 0, Vector2.Zero));
+            animator.CreateAnimation("WalkFront", new Animation(4, 100, 0, 100, 100, 5, Vector2.Zero));
+            animator.CreateAnimation("WalkBack", new Animation(4, 100, 4, 100, 100, 5, Vector2.Zero));
+            animator.CreateAnimation("WalkLeft", new Animation(4, 200, 0, 100, 100, 5, Vector2.Zero));
+            animator.CreateAnimation("WalkRight", new Animation(4, 200, 4, 100, 100, 5, Vector2.Zero));
+            animator.CreateAnimation("DieBack", new Animation(4, 300, 0, 100, 100, 5, Vector2.Zero));
+            animator.CreateAnimation("DieFront", new Animation(4, 300, 4, 100, 100, 5, Vector2.Zero));
+            animator.CreateAnimation("DieLeft", new Animation(4, 400, 0, 100, 100, 5, Vector2.Zero));
+            animator.CreateAnimation("DieRight", new Animation(4, 400, 4, 100, 100, 5, Vector2.Zero));
+            animator.PlayAnimation("IdleFront");
+        }
+
+        public void OnAnimationDone(string animationName)
+        {
+           
+        }
+
+        public void Update()
+        {
+            if (strategy is Walk)
+            {
+
+            }
+            if (strategy is Idle)
+            {
+
+            }
+            if (strategy is Attack)
+            {
+
+            }
+            EnemyMovement();
+        }
+        public void EnemyMovement()
+        {
+            gameObject.Transform.Translate(new Vector2(+2, 0));
         }
         #endregion
     }
