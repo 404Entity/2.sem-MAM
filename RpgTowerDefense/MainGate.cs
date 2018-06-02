79976349c24b1eae43ce66c1f4bc6769cfc22f94
Morten Ellegaard Hansen <mort140w@student.eadania.dk>
2018-06-02 15:13:29 +0200

    f

--- RpgTowerDefense/MainGate.cs
+++ RpgTowerDefense/MainGate.cs
@@ -29,7 +29,7 @@ namespace RpgTowerDefense
         #region Constructer
         public MainGate(GameObject gameObject): base(gameObject)
         {
-            GateHealth gateHealth = new GateHealth();
+            
 
 
 
