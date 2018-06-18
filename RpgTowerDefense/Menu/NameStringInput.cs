83d105c9fe73282087fc15e38ecc8fddd560217a
Morten Ellegaard Hansen <mort140w@student.eadania.dk>
2018-06-18 14:07:32 +0200

    Nameinput

---
+++ RpgTowerDefense/Menu/NameStringInput.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RpgTowerDefense
+{
+    class NameStringInput : MenuBase
+    {
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
